fee02a915cc5c08ce4e5063dd60da86712d3cc94
James Brown <james@catch22.net>
2012-06-08 12:34:40 -0400

    Added version.txt

--- build/buildzip.rb
+++ build/buildzip.rb
@@ -6,7 +6,7 @@ require 'pp'
 $ignore  = [ '.git' ]
 $bindir = '../bin/i386-pc-win32/Unicode_Release'
 
-def zipfiles(zipfile, filespec, dest, ignore=[])
+def zipfile(zipfile, filespec, dest, ignore=[])
 
   spec = filespec
   spec = "#{filespec}/**/**" if File.directory?(filespec)
@@ -21,13 +21,24 @@ def zipfiles(zipfile, filespec, dest, ignore=[])
   end
 end
 
+def ziptext(zipfile, dest)
+
+ puts "Adding:    #{dest}"
+
+ zipfile.get_output_stream(dest) do |f|
+      yield f
+ end  
+
+end
+
 def version(file)
 
   s = ""
-  v = '0.0.0'
 
   # get the amount of space required to read in the version info
   vsize = Win32API.new('version.dll', 'GetFileVersionInfoSize', ['P', 'P'], 'L').call(file, s)
+
+  ver = {}
 
   if vsize > 0
     result = ' ' * vsize
@@ -38,44 +49,53 @@ def version(file)
     rstring = result.unpack('v*').map{ |s| s.chr if s < 256} * ''
 
     r = /FileVersion\D+([\d\.]+)\000/.match(rstring)
-    puts "Packaging: #{file} - version #{r[1]}" if r
+    ver[:filever] = r[1] if r
 
     r = /ProductVersion\D+([\d|\.]+)/.match(rstring)
-    v = r[1] if r
+    ver[:prodver] = r[1] if r
 
   end
 
-  return v  
+  return ver
 end
 
 def main()
 
+  hexbin = File.join($bindir, 'HexEdit.exe')
+
   # get the fileversion from exe resource section
-  ver = version(File.join($bindir, 'HexEdit.exe'))
+  ver = version(hexbin)
+
+  puts "Packaging: #{hexbin} - version #{ver[:filever]}" 
 
   begin
     Dir.mkdir('out')
   rescue
   end
-  zipname = "out/hexedit-#{ver}.zip"
-
-  puts "Building:  #{zipname}"
+  zipname = "out/hexedit-#{ver[:prodver]}.zip"
 
   # delete any existing zip
   FileUtils.rm zipname, :force=>true
 
   # build the zip!
-  Zip::ZipFile.open(zipname, Zip::ZipFile::CREATE) do |zipfile|
+  Zip::ZipFile.open(zipname, Zip::ZipFile::CREATE) do |zf|
 
-    zipfiles(zipfile, '../README.TXT',                   'HexEdit/README.TXT',  $ignore)
-    zipfiles(zipfile, '../LICENCE.TXT',                  'HexEdit/LICENCE.TXT', $ignore)
-    zipfiles(zipfile, File.join($bindir, 'HexEdit.exe'), 'HexEdit/HexEdit.exe', $ignore)
-    zipfiles(zipfile, File.join($bindir, 'typelib'),     'HexEdit/typelib',     $ignore)
-
+    zipfile(zf, '../README.TXT',                   'HexEdit/README.TXT',  $ignore)
+    zipfile(zf, '../LICENCE.TXT',                  'HexEdit/LICENCE.TXT', $ignore)
+    zipfile(zf, File.join($bindir, 'HexEdit.exe'), 'HexEdit/HexEdit.exe', $ignore)
+    zipfile(zf, File.join($bindir, 'typelib'),     'HexEdit/typelib',     $ignore)
+    ziptext(zf, 'HexEdit/VERSION.TXT') do |f| 
+      f.puts "HexEdit #{ver[:filever]}\r" 
+      f.puts "Built on: #{Time.now.strftime('%Y/%m/%d %H:%M:%S')}\r"
+    end
   end
 
   len = File.size(zipname)
-  puts "Done:      #{len} bytes"
+  puts ""
+  puts "Done:      #{zipname}"
+  puts "           #{len} bytes"
+  puts ""
+
 end
 
 main()
